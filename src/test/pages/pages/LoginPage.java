bd13fcb8085c34ae4fa5347f3a1af97cfdb87108
Sofia Arguello <75380684+sofiaarguello@users.noreply.github.com>
2026-05-11 19:18:53 -0300

    Add LoginPage class for user authentication
    Implemented LoginPage class with methods for user login and error handling.

--- src/test/pages/pages/LoginPage.java
+++ src/test/pages/pages/LoginPage.java
@@ -1,1 +1,60 @@
+package pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage extends BasePage {
+
+    @FindBy(id = "user-name")
+    private WebElement usernameInput;
+
+    @FindBy(id = "password")
+    private WebElement passwordInput;
+
+    @FindBy(id = "login-button")
+    private WebElement loginButton;
+
+    @FindBy(css = "[data-test='error']")
+    private WebElement errorMessage;
+
+    public LoginPage(WebDriver driver) {
+        super(driver);
+        PageFactory.initElements(driver, this);
+    }
+
+    public void open() {
+        driver.get("https://www.saucedemo.com/");
+    }
+
+    public void enterUsername(String username) {
+        type(usernameInput, username);
+    }
+
+    public void enterPassword(String password) {
+        type(passwordInput, password);
+    }
+
+    public void clickLogin() {
+        click(loginButton);
+    }
+
+    public void login(String username, String password) {
+        enterUsername(username);
+        enterPassword(password);
+        clickLogin();
+    }
+
+    public String getErrorMessage() {
+        return getText(errorMessage);
+    }
+
+    public boolean isErrorDisplayed() {
+        try {
+            return errorMessage.isDisplayed();
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
